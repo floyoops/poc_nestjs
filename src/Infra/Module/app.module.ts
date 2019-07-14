bc68e546b2a20922f999f78416f13a583faa8148
Florian Chaudret <florian.chaudret@gmail.com>
2019-07-14 14:03:06 +0200

    Feature init connection database
    prerequisites a database poc_nestjs

--- src/Infra/Module/app.module.ts
+++ src/Infra/Module/app.module.ts
@@ -3,12 +3,25 @@ import { AppController } from '../../UI/Http/Rest/app.controller';
 import { AppService } from '../Service/app.service';
 import {ArticleService} from '../Service/ArticleService';
 import {CqrsModule} from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import {CreateAnArticleCommandHandlerService} from '../Command/CreateAnArticleCommandHandlerService';
 
 export const commandHandlers = [CreateAnArticleCommandHandlerService];
 
 @Module({
-  imports: [CqrsModule],
+  imports: [
+      CqrsModule,
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'toor',
+        database: 'poc_nestjs',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
