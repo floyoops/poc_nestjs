8914df11a21f660cfe0a8a67e2640ba4b9f85b98
Florian Chaudret <fchaudret@leadersleague.com>
2019-07-22 18:36:48 +0200

    Generate uuid

--- src/ui/http/rest/app.controller.ts
+++ src/ui/http/rest/app.controller.ts
@@ -1,6 +1,7 @@
 import {Controller, Get, HttpException, Inject} from '@nestjs/common';
 import {ArticleService} from '../../../modules/article/infra/service/article.service';
 import {CreateAnArticleCommand} from '../../../modules/article/application/command/create-an-article.command';
+import v4 = require('uuid/v4');
 
 @Controller()
 export class AppController {
@@ -11,7 +12,8 @@ export class AppController {
   @Get()
   async  getHello() {
     try {
-        const command = new CreateAnArticleCommand('ab358607-3e72-4fb7-9eed-b1866a1b12e3', 'Hello World!');
+        const uuid: string = AppController.getNewUuid();
+        const command = new CreateAnArticleCommand(uuid, 'Hello World!');
         return await this.articleService.article(command);
     } catch (e) {
         throw new HttpException(e, 500);
@@ -25,5 +27,9 @@ export class AppController {
       } catch (e) {
           throw new HttpException(e, 500);
       }
+  }
+
+  private static getNewUuid(): string {
+      return v4();
   }
 }
