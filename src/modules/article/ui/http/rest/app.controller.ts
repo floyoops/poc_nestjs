657592190bb4e9d8d8d098887cef6b2c8ecf3ed1
Florian Chaudret <fchaudret@leadersleague.com>
2019-07-23 14:16:29 +0200

    wip

--- src/modules/article/ui/http/rest/app.controller.ts
+++ src/modules/article/ui/http/rest/app.controller.ts
@@ -1,9 +1,10 @@
-import {Controller, Get, HttpException, Inject, Param} from '@nestjs/common';
+import {Body, Controller, Get, HttpException, HttpStatus, Inject, Param, Post, Res} from '@nestjs/common';
 import {ArticleService} from '../../../infra/service/article.service';
-import {CreateAnArticleCommand} from '../../../application/command/create-an-article.command';
 import v4 = require('uuid/v4');
 import {FindOneArticleQuery} from '../../../application/query/find-one-article.query';
 import {HttpArticleNotFoundException} from './exception/http-article-not-found.exception';
+import {CreateAnArticleDto} from './dto/create-an-article.dto';
+import {CreateAnArticleCommand} from '../../../application/command/create-an-article.command';
 
 @Controller('article')
 export class AppController {
@@ -11,14 +12,13 @@ export class AppController {
         @Inject(ArticleService) private readonly articleService: ArticleService,
     ) {}
 
-    @Get('/new')
-    async  new() {
+    @Post('/')
+    async  new(@Body() createAnArticleDto: CreateAnArticleDto) {
         try {
-            const uuid: string = AppController.getNewUuid();
-            const command = new CreateAnArticleCommand(uuid, 'Hello World!');
+            const command = new CreateAnArticleCommand(AppController.getNewUuid(), createAnArticleDto.title);
             return await this.articleService.article(command);
         } catch (e) {
-            throw new HttpException(e, 500);
+            throw new HttpException(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -27,12 +27,8 @@ export class AppController {
         try {
             return await this.articleService.findAll();
         } catch (e) {
-            throw new HttpException(e, 500);
+            throw new HttpException(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
-
-    private static getNewUuid(): string {
-        return v4();
     }
 
     @Get('/:uuid')
@@ -41,11 +37,15 @@ export class AppController {
             const query = new FindOneArticleQuery(uuid);
             const article = await this.articleService.findOne(query);
             if (article === undefined) {
-                throw new HttpArticleNotFoundException('Article not found', 404);
+                throw new HttpArticleNotFoundException('Article not found', HttpStatus.NOT_FOUND);
             }
             return article;
         } catch (e) {
-            throw new HttpException(e, 500);
+            throw new HttpException(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private static getNewUuid(): string {
+        return v4();
     }
 }
