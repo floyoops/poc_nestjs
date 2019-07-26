70f8c66420aae78f4014820941cd54271381c051
Florian Chaudret <fchaudret@leadersleague.com>
2019-07-26 18:33:15 +0200

    Init module fixtures services

---
+++ src/modules/fixtures/fixtures.service.ts
@@ -0,0 +1,12 @@
+import {Inject, Injectable} from '@nestjs/common';
+import {FakerService} from '../faker/faker.service';
+import {ArticleService} from '../article/infra/service/article.service';
+
+@Injectable()
+export class FixturesService {
+    constructor(
+        @Inject(FakerService) private readonly fakerService: FakerService,
+        @Inject(ArticleService) private readonly articleService: ArticleService,
+    ) {
+    }
+}
